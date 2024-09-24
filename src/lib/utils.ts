671148ebe73d46bd7a9eb782348780dc5e14d2fa
valmojr <val_mo_jr@hotmail.com>
2024-09-24 04:52:37 -0300

    adde function to parse the token to get user

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,13 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import jwt from "jsonwebtoken";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function getUserFromToken(token: string) {
+  return jwt.verify(token, process.env.JWT_SECRET || "", {
+    complete: false,
+  });
 }
