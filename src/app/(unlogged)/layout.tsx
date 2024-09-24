b8f28011885e4d92ea0d8dc61f7fe1ba0ee6902b
valmojr <val_mo_jr@hotmail.com>
2024-09-24 14:03:47 -0300

    trying to fix the seamless border on botton

--- src/app/(unlogged)/layout.tsx
+++ src/app/(unlogged)/layout.tsx
@@ -6,7 +6,7 @@ export default function UnloggedLayout({
   children: React.ReactNode;
 }) {
   return (
-    <section className="flex flex-row flex-nowrap h-screen w-full">
+    <section className="flex flex-row flex-nowrap h-full w-full">
       <div className="bg-secondary lg:w-full flex flex-col flex-nowrap items-center justify-center w-0">
         <div className="text-end">
           <h1 className="italic lg:text-3xl text-[0px]">
