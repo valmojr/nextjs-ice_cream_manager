322bb5cfecfb1d439c2611f7e1641a813b83cb9d
valmojr <val_mo_jr@hotmail.com>
2024-09-24 02:20:49 -0300

    updated cookie parser on root route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
 export default function Home() {
-  const authentication = cookies().get('token')?.value;
+  const authentication = cookies().get('authToken')?.value;
 
   return authentication ? redirect('/main') : redirect('login');
 }
