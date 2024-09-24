376da9bb6fb090d0b8cf791e7a4055f990626c1c
valmojr <val_mo_jr@hotmail.com>
2024-09-24 04:54:16 -0300

    removed useless imports

--- src/app/(logged)/layout.tsx
+++ src/app/(logged)/layout.tsx
@@ -1,13 +1,7 @@
-import { ModeToggle } from "@/components/mode-toggle";
-import { Avatar, AvatarFallback } from "@/components/ui/avatar";
-import { Card } from "@/components/ui/card";
-import { Separator } from "@/components/ui/separator";
 import { getUserFromToken } from "@/lib/utils";
 import { User } from "@prisma/client";
-import { Key, LayoutDashboard, Package2, SeparatorHorizontal, Users } from "lucide-react";
 import { cookies } from "next/headers";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
 import LoggedLayoutTopMenu from "./topmenu";
 
 export default function loggedLayout({
