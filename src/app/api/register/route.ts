fc25bf792a7ae6d16732fc03a98ffefbc55a9c10
valmojr <val_mo_jr@hotmail.com>
2024-10-02 18:57:15 -0300

    added handling for login route endpoint

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -3,37 +3,44 @@ import prisma from "@/lib/database";
 import { randomUUID } from "crypto";
 
 type ImputedUser = {
-  username: string,
-  email: string,
-  displayname: string,
-  password: string,
-  confirmPassword: string
-}
+  username: string;
+  email: string;
+  displayname: string;
+  password: string;
+  confirmPassword: string;
+};
 
 export async function POST(request: Request) {
-  const body = await request.json();
+  try {
+    const body = await request.json();
 
-  const { confirmPassword, ...data}: ImputedUser = body;
+    const { confirmPassword, ...data }: ImputedUser = body;
 
-  const newUser = await prisma.user.create({
-    data: {
-      id: randomUUID(),
-      createdAt: new Date(),
-      updatedAt: new Date(),
-      ...data,
-      password: await hashPassword(data.password)
+    const newUser = await prisma.user.create({
+      data: {
+        id: randomUUID(),
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        ...data,
+        password: await hashPassword(data.password),
+      },
+    });
+
+    if (newUser.id) {
+      return Response.json({
+        status: 201,
+        user: newUser,
+      });
+    } else {
+      return Response.json({
+        status: 400,
+        error: "Failed to register the User",
+      });
     }
-  })
-
-  if (newUser.id) {
-    return Response.json({
-      status: 201,
-      user: newUser
-    })
-  } else {
+  } catch (error) {
     return Response.json({
       status: 400,
-      error: 'Failed to register the User'
-    })
+      error: "Bad Request - " + error,
+    });
   }
-};
+}
