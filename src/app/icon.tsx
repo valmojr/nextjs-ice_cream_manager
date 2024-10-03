b902258dde41812e8c7a6d69461bfdc6cf26f823
valmojr <val_mo_jr@hotmail.com>
2024-10-03 18:55:55 -0300

    added tsx icon

---
+++ src/app/icon.tsx
@@ -0,0 +1,36 @@
+import { IceCreamBowl } from 'lucide-react'
+import { ImageResponse } from 'next/og'
+
+// Image metadata
+export const size = {
+  width: 32,
+  height: 32,
+}
+export const contentType = 'image/png'
+
+// Image generation
+export default function Icon() {
+  return new ImageResponse(
+    (
+      // ImageResponse JSX element
+      <div
+        style={{
+          fontSize: 24,
+          background: 'white',
+          color: 'black',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          borderRadius: '9999px',
+        }}
+      >
+        G
+      </div>
+    ),
+    {
+      ...size,
+    }
+  )
+}
