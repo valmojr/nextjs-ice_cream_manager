e9352546f47e6d62453bfd3d1eeb48a99e8404d4
valmojr <val_mo_jr@hotmail.com>
2024-10-04 03:35:55 -0300

    added storage authorization check

--- src/app/(logged)/storage/page.tsx
+++ src/app/(logged)/storage/page.tsx
@@ -1,7 +1,11 @@
+import { getUserFromToken } from "@/lib/utils";
 import ProductList from "./ProductList"
 import StorageCount from "./StorageCount"
+import { RoledUser as User } from "@/lib/types";
+import { cookies } from "next/headers";
 
 export default function StorageHistoryPage() {
+  const user = getUserFromToken(cookies().get('authToken')?.value as string) as User;
   return <>
     <ProductList/>
     <StorageCount/>
