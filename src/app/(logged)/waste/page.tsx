bcc4248de1416ceed62b94bd19ad21703ed4abbf
valmojr <val_mo_jr@hotmail.com>
2024-10-04 14:13:24 -0300

    changed type on user waste

--- src/app/(logged)/waste/page.tsx
+++ src/app/(logged)/waste/page.tsx
@@ -2,8 +2,8 @@ import { cn, getUserFromToken } from "@/lib/utils";
 import WasteChart from "./wasteChart";
 import prisma from "@/lib/database";
 import { cookies } from "next/headers";
-import { User } from "@prisma/client";
 import WasteForm from "./wasteForm";
+import { RoledUser as User } from "@/lib/types";
 
 export default async function WastePage() {
   const products = (await prisma.product.findMany()).map(product => {
