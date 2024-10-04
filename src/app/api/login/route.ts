19d78124c2d99478d5655312a73448abe31b46ab
valmojr <val_mo_jr@hotmail.com>
2024-10-04 14:10:50 -0300

    added storeId on jwt payload

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -33,6 +33,7 @@ export async function POST(request: Request) {
       });
 
       return {
+        storeId: store?.id as string,
         storeName: store?.name as string,
         function: role.function,
       };
