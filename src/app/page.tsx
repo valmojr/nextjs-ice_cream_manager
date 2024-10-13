e770913cec5bbd7c84d22734f56b2377ecd7359e
valmojr <val_mo_jr@hotmail.com>
2024-10-12 23:59:52 -0300

    added auth check on main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 import { checkToken } from "@/lib/CheckToken";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return checkToken();
+  checkToken();
+
+  const authentication = cookies().get("authToken")?.value;
+  authentication ? redirect("/dashboard") : redirect("/login");
 }
