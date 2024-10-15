e2af95fd302f24fe72e6f9bc0b30ddbdc311d9e4
valmojr <val_mo_jr@hotmail.com>
2024-10-15 18:50:13 -0300

    added error handling for user page

--- src/app/(logged)/user/[id]/page.tsx
+++ src/app/(logged)/user/[id]/page.tsx
@@ -1,7 +1,11 @@
 import { getUserById } from "@/app/api/user/functions";
 
 export default async function UserPage({ params }: { params: { id: string } }) {
-  const user = await getUserById(params.id);
+  try {
+    const user = await getUserById(params.id);
 
-  return <h1>{user.displayname}</h1>
+    return <h1>{user.displayname}</h1>
+  } catch (error) {
+    return <p>Failed to retrieve User {params.id} information</p>
+  }
 }
