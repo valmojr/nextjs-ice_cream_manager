90f235ab5640fd9264cdb2168db4df3002172c0e
valmojr <val_mo_jr@hotmail.com>
2024-10-31 17:45:10 -0300

    made initial state of storage counter

--- src/app/(logged)/storage/StorageCountForm.tsx
+++ src/app/(logged)/storage/StorageCountForm.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { cn } from "@/lib/utils";
-import { Store } from "@prisma/client";
+import { Product, Store } from "@prisma/client";
 import { useState } from "react";
 import {
   Select,
@@ -14,17 +14,77 @@ import { z } from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
-import Link from "next/link";
 import { Button } from "@/components/ui/button";
+import { MinusCircle, PlusCircle } from "lucide-react";
 
-export default function StorageCountForm({ stores }: { stores: Store[] }) {
+type ProductCount = {
+  productId: number;
+  count: number;
+};
+
+
+export default function StorageCountForm({ stores, products }: { stores: Store[], products: Product[] }) {
   const selectStoreSchema = z.object({
     selectedStore: z.string({ message: "Selecione uma loja para continuar" }),
   });
   const selectStoreForm = useForm<z.infer<typeof selectStoreSchema>>({
     resolver: zodResolver(selectStoreSchema),
-  })
+  });
   const [selectedStore, setSelectedStore] = useState<Store | null>(null);
+
+  products = products.filter((product) => {
+    return product.countable;
+  })
+
+  const [productCount, setProductCount] = useState<ProductCount[]>([]);
+
+  function ProductFrame({ product }: { product: Product }) {
+    const currentCount = productCount.find((count) => count.productId === product.id)?.count || 0;
+    function addCount() {
+      if (currentCount > 0) {
+        setProductCount(productCount.map((count) => {
+          if (count.productId === product.id) {
+            return { ...count, count: count.count + 1 };
+          }
+          return count;
+        }));
+      } else {
+        setProductCount([...productCount, { productId: product.id, count: 1 }]);
+      }
+    }
+
+    function subtractCount() {
+      if (currentCount > 0) {
+        setProductCount(productCount.map((count) => {
+          if (count.productId === product.id) {
+            return { ...count, count: count.count - 1 };
+          }
+          return count;
+        }));
+      }
+    }
+
+    return <div className={cn(
+      "flex flex-row flex-nowrap justify-between items-center",
+      "w-full h-fit px-8 py-4",
+      "border")}>
+      <h1 className="text-2xl">{product.name}</h1>
+      <div className="flex flex-row gap-4">
+        <div className={cn("text-2xl w-20 h-14 bg-foreground text-background rounded-md", " flex flex-row flex-nowrap items-center justify-center")}>
+          {currentCount}
+        </div>
+        <button onClick={addCount}><PlusCircle size={48} /></button>
+        {currentCount > 0 ?
+          <button onClick={subtractCount}><MinusCircle size={48} /></button> :
+          <button disabled><MinusCircle size={48} className="stroke-zinc-500" /></button>
+        }
+      </div>
+    </div>
+  }
+
+  function submitCount() {
+    console.log(productCount);
+  }
 
   function onSubmitStoreSelection(data: z.infer<typeof selectStoreSchema>) {
     const foundSelectedStore = stores.find((store) => store.id === data.selectedStore) as Store;
@@ -36,7 +96,15 @@ export default function StorageCountForm({ stores }: { stores: Store[] }) {
     "w-full h-full lg:max-w-[600px]",
     "border")}>
     {(selectedStore ? (<>
-      <h1>Hello there</h1>
+      {products.map((product) => (
+        <ProductFrame key={product.id} product={product} />
+      ))}
+      <Button
+        onClick={submitCount}
+        className={cn("h-12 w-48 mt-4", "bg-foreground text-background text-2xl")}
+      >
+        Confirmar
+      </Button>
     </>) : (<Form {...selectStoreForm}>
       <form onSubmit={selectStoreForm.handleSubmit(onSubmitStoreSelection)} className="w-2/3 space-y-6">
         <FormField
--- src/app/(logged)/storage/page.tsx
+++ src/app/(logged)/storage/page.tsx
@@ -4,12 +4,15 @@ import StorageCount from "./StorageCount"
 import { RoledUser as User } from "@/lib/types";
 import { cookies } from "next/headers";
 import { getAllStores } from "@/app/api/store/functions";
+import { getAllProducts } from "@/app/api/product/functions";
 
 export default async function StorageHistoryPage() {
   const user = getUserFromToken(cookies().get('authToken')?.value as string) as User;
   const userStores = await getAllStores(user.id);
+  const products = await getAllProducts();
+
   return <>
-    <StorageCountForm stores={userStores} />
+    <StorageCountForm stores={userStores} products={products} />
     <StorageCount/>
   </>
 }
