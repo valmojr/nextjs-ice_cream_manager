0739469b178c9dafd8725391eccaa504db97181c
valmojr <val_mo_jr@hotmail.com>
2024-11-04 21:20:13 -0300

    [WIP] storage count update

--- src/app/(logged)/storage/StorageCount.tsx
+++ src/app/(logged)/storage/StorageCount.tsx
@@ -1,9 +1,12 @@
 import { getAllStorageCountByStoreId } from "@/app/api/storageCount/functions";
-import { Card } from "@/components/ui/card";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { cn } from "@/lib/utils";
 
 export default async function StorageCount() {
   const mostRecentStorageCount = await getAllStorageCountByStoreId("1");
-  return <Card className="lg:w-full w-[0px]">
-    <h1 className="lg:text-2xl text-[0px]">{JSON.stringify(mostRecentStorageCount)}</h1>
-  </Card>
-  }
+  return <ScrollArea className={cn("lg:w-full w-[0px] lg:h-full h-[0px] border")}>
+    <h1 className="lg:text-2xl text-[0px]">
+      {JSON.stringify(mostRecentStorageCount)}
+    </h1>
+  </ScrollArea>
+}
--- src/app/api/store/functions.ts
+++ src/app/api/store/functions.ts
@@ -1,7 +1,7 @@
 import prisma from "@/lib/database";
 
-export function getAllStores(userId: string) {
-  return prisma.store.findMany({
+export async function getAllStores(userId: string) {
+  return await prisma.store.findMany({
     where: {
       roles: {
         some: {
